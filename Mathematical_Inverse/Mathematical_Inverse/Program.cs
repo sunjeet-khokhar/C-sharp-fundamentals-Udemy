9627bac330be64dfb3ee0a57035f42f23202a93a
sunjeet-khokhar <sunjeet81@gmail.com>
2017-08-17 14:45:55 +1200

    mathematical inverse exercise

---
+++ Mathematical_Inverse/Mathematical_Inverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematical_Inverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test_arr = new int[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 };
+            math_inverse(test_arr);
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+        }
+
+        public static void math_inverse(int[] seq)
+        {
+            for (int i = 0; i < (seq.Length); i++)
+            {
+                seq[i] = -(seq[i]);
+                Console.WriteLine(seq[i]);
+            }
+        }
+    }
+}
